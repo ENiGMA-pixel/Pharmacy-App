ce3a4a523d1bbc9a3688dcceb6bc68302c32f836
Pranab Kumar Agasti <101541133+PranabKumar1527@users.noreply.github.com>
2023-04-18 23:28:39 +0530

    Add files via upload

---
+++ src/main/java/com/example/billingactivity/NetBankingActivity.java
@@ -0,0 +1,14 @@
+package com.example.billingactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NetBankingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net_banking);
+    }
+}
