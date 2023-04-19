23b8bb6819322ac505174699b738367aa433590f
Swetaprava Patasani <sweta10222>
2023-04-20 01:47:23 +0530

    Physician and Dietitian added

--- app/src/main/java/com/example/pharmacy_app/Dietitian.java
+++ app/src/main/java/com/example/pharmacy_app/Dietitian.java
@@ -2,7 +2,13 @@ package com.example.pharmacy_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Dietitian extends AppCompatActivity {
 
@@ -10,5 +16,24 @@ public class Dietitian extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dietitian);
+
+        Button back = findViewById(R.id.back);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if(user == null) {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        }
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),FindDoctor.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/pharmacy_app/Physician.java
+++ app/src/main/java/com/example/pharmacy_app/Physician.java
@@ -2,13 +2,44 @@ package com.example.pharmacy_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Physician extends AppCompatActivity {
-
+    Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_physician);
+
+        back = findViewById(R.id.back);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if(user == null) {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        }
+        
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),FindDoctor.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
